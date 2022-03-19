f4a8e95206ba3bfe440ee49594feaea17600d0a8
damien911224 <damien911224@gmail.com>
2022-03-19 18:36:11 +0900

    Modified

--- core/update.py
+++ core/update.py
@@ -168,8 +168,6 @@ class Decoder(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(hidden_dim, 2, 1, padding=0))
 
-        self._reset_parameters()
-
     def forward(self, query, key):
         q_n, q_c, q_h, q_w = query.size()
         k_n, k_c, k_h, k_w = key.size()

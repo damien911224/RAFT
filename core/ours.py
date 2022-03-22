99d06465defabe49edc86f18d7ec926c42995bfd
damien911224 <damien911224@gmail.com>
2022-03-22 22:02:41 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -40,7 +40,7 @@ class RAFT(nn.Module):
         self.fnet = BasicEncoder(output_dim=128, norm_fn="batch", dropout=args.dropout)
 
         d_model = 64
-        num_feature_levels = 3
+        num_feature_levels = 1
         self.num_feature_levels = num_feature_levels
         h, w = args.image_size[0], args.image_size[1]
         self.row_pos_embed = nn.ModuleList([nn.Embedding(w // (2 ** i), d_model // 2)

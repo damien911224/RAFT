329e13817bd32bb65010a23594ff966ff5c4791a
damien911224 <damien911224@gmail.com>
2022-03-23 00:51:48 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -178,6 +178,7 @@ class RAFT(nn.Module):
                 print(reference.shape)
                 print(init_reference[prev_idx:prev_idx + this_len].shape)
                 print(tmp[prev_idx:prev_idx + this_len].shape)
+                print(this_len)
                 flow = tmp[prev_idx:prev_idx + this_len] + reference
                 flow = init_reference[prev_idx:prev_idx + this_len] - flow.sigmoid()
                 flow = flow.view(bs, h, w, 2).permute(0, 3, 1, 2)

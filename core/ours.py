bed0b2ef4a707aea22f4d03fc99a34d2ac495549
damien911224 <damien911224@gmail.com>
2022-03-22 22:21:58 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -57,7 +57,7 @@ class RAFT(nn.Module):
         self.reset_parameters()
 
         self.transformer = DeformableTransformer(d_model=d_model, nhead=8,
-                                                 num_encoder_layers=3, num_decoder_layers=3,
+                                                 num_encoder_layers=6, num_decoder_layers=6,
                                                  dim_feedforward=d_model * 4, dropout=0.1,
                                                  activation="relu", return_intermediate_dec=True,
                                                  num_feature_levels=num_feature_levels, dec_n_points=4, enc_n_points=4)

b2a8e7995c4769101e554e88c62a7f9574a8c94a
damien911224 <damien911224@gmail.com>
2022-03-24 13:50:01 +0900

    Modified

--- core/ours_02.py
+++ core/ours_02.py
@@ -76,6 +76,9 @@ class RAFT(nn.Module):
 
         self.flow_embed = MLP(d_model, d_model, 2, 3)
         self.corr_embed = MLP(d_model, d_model, d_model, 3)
+        # self.context_embed = nn.Sequential(
+        #         nn.Conv2d(d_model, d_model, kernel_size=1),
+        #         nn.GroupNorm(d_model // 2, d_model))
         self.context_embed = nn.Sequential(
                 nn.Conv2d(d_model, d_model, kernel_size=1),
                 nn.GroupNorm(d_model // 2, d_model))

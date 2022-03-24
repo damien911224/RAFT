7b61c8dd9a983cbc8064492ee9300403c9c9b902
damien911224 <damien911224@gmail.com>
2022-03-24 09:51:35 +0900

    Modified

--- core/ours_02.py
+++ core/ours_02.py
@@ -101,15 +101,15 @@ class RAFT(nn.Module):
             nn.init.xavier_uniform_(proj[0].weight, gain=1)
             nn.init.constant_(proj[0].bias, 0)
 
-        for p in self.flow_embed:
+        for p in self.flow_embed.layers:
             nn.init.xavier_uniform_(p.weight, gain=1)
             nn.init.constant_(p.bias, 0)
 
-        for p in self.corr_embed:
+        for p in self.corr_embed.layers:
             nn.init.xavier_uniform_(p.weight, gain=1)
             nn.init.constant_(p.bias, 0)
 
-        for p in self.context_embed:
+        for p in self.context_embed.layers:
             nn.init.xavier_uniform_(p.weight, gain=1)
             nn.init.constant_(p.bias, 0)
 

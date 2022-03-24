98fabdebe969478e8c4b07b3a4ed747c7b13dc61
damien911224 <damien911224@gmail.com>
2022-03-24 22:36:01 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -207,7 +207,7 @@ class RAFT(nn.Module):
                     flow *= torch.tensor((i_h, i_w), dtype=torch.float32).view(1, 2, 1, 1).to(flow.device)
                     flow = F.interpolate(flow, size=(i_h, i_w), mode="bilinear", align_corners=True)
 
-                    this_flow.append(tf.stack((flow, corr_flow), axis=-1))
+                    this_flow.append(torch.stack((flow, corr_flow), axis=-1))
                     split = 0
                     # flow = tmp[:, prev_idx:prev_idx + this_len]
                     # flow = flow.view(bs, h * w, c)

7f460728cd725bf59f8fe0bfe3c78fceb0115fe3
damien911224 <damien911224@gmail.com>
2022-03-24 22:10:00 +0900

    Modified

--- core/deformable.py
+++ core/deformable.py
@@ -179,6 +179,7 @@ class DeformableTransformer(nn.Module):
         # bs, h * w + n, c
         prop_total_tgt_embed = torch.cat((prop_tgt_embed, prop_tgt_N_query), dim=1)
         prop_N_reference_points = self.prop_N_reference_points(prop_tgt_N_query_pos).sigmoid()
+        print(reference_points.shape, prop_N_reference_points.shape)
         prop_reference_points = torch.cat((reference_points, prop_N_reference_points), dim=1)
         prop_pos = torch.cat((lvl_pos_embed_flatten, prop_tgt_N_query_pos), dim=1)
         prop_hs, prop_inter_references = self.prop_decoder(prop_total_tgt_embed, prop_reference_points, memory_01,

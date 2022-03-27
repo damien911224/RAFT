f373b7b36c76601cb852337f0439fc8f7e0a6ccf
damien911224 <damien911224@gmail.com>
2022-03-28 00:16:12 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -243,7 +243,7 @@ class RAFT(nn.Module):
                 extractor_flow = torch.bmm(U1, correlation_context.permute(0, 2, 1))
                 # bs, HW, 2
                 # extractor_flow = torch.bmm(extractor_flow, context_flow)
-                extractor_flow = torch.bmm(extractor_flow, correlation_flow.detach())
+                extractor_flow = torch.bmm(extractor_flow, correlation_flow)
                 # bs, 2, H, W
                 flow = torch.tanh(extractor_flow.permute(0, 2, 1).view(bs, 2, H, W))
 

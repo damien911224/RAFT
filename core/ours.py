652df106af5b72d22ef98c2894f4f514b74b1423
damien911224 <damien911224@gmail.com>
2022-04-08 20:45:37 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -210,7 +210,7 @@ class RAFT(nn.Module):
             query_pos = self.query_pos_embed.weight.unsqueeze(0).repeat(bs, 1, 1)
             # reference_points = self.query_ref_embed.weight.unsqueeze(0).repeat(bs, 1, 1).unsqueeze(2)
 
-            init_reference_points = self.get_reference_points([(10, 10), ], device=D1.device).squeeze(2)
+            # init_reference_points = self.get_reference_points([(10, 10), ], device=D1.device).squeeze(2)
 
             spatial_shapes = torch.as_tensor([(h, w), ] * 2, dtype=torch.long, device=D1.device)
             level_start_index = torch.cat((spatial_shapes.new_zeros((1, )), spatial_shapes.prod(1).cumsum(0)[:-1]))

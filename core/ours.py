5a1f90a2dbcfe18d29538ab79c74d1a6a5439503
damien911224 <damien911224@gmail.com>
2022-04-09 00:26:29 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -260,7 +260,7 @@ class RAFT(nn.Module):
                 # context_flow = F.softmax(torch.bmm(U1, context.permute(0, 2, 1)), dim=-1)
                 context_flow = torch.sigmoid(torch.bmm(U1, context.permute(0, 2, 1)))
                 # bs, HW, 2
-                context_flow = torch.bmm(context_flow, flow)
+                context_flow = torch.bmm(context_flow, flow.detach())
                 # bs, 2, H, W
                 context_flow = context_flow.permute(0, 2, 1).view(bs, 2, H, W)
 

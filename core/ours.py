c44c8488f2cd375eb89c75d726ba128d2ce38793
damien911224 <damien911224@gmail.com>
2022-04-13 01:10:51 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -311,8 +311,8 @@ class RAFT(nn.Module):
                 # reference_points = reference_points.unsqueeze(2).sigmoid()
 
                 # bs, HW, n
-                # context_flow = F.softmax(torch.bmm(U1, context.permute(0, 2, 1)), dim=-1)
-                context_flow = torch.sigmoid(torch.bmm(U1, context.permute(0, 2, 1)))
+                context_flow = F.softmax(torch.bmm(U1, context.permute(0, 2, 1)), dim=-1)
+                # context_flow = torch.sigmoid(torch.bmm(U1, context.permute(0, 2, 1)))
                 # bs, HW, 2
                 context_flow = torch.bmm(context_flow, flow)
                 # bs, 2, H, W

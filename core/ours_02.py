90acef4da598e4cd55cc5716fef137c4c5b41708
damien911224 <damien911224@gmail.com>
2022-04-13 19:25:49 +0900

    Modified

--- core/ours_02.py
+++ core/ours_02.py
@@ -187,14 +187,14 @@ class MLP(nn.Module):
         super().__init__()
         self.num_layers = num_layers
         h = [hidden_dim] * (num_layers - 1)
-        # self.layers = nn.ModuleList(nn.Linear(n, k) for n, k in zip([input_dim] + h, h + [output_dim]))
-        self.layers = nn.ModuleList(nn.Conv1d(n, k, kernel_size=1) for n, k in zip([input_dim] + h, h + [output_dim]))
-        self.norms = nn.ModuleList([nn.GroupNorm(c // 2, c) if c is not None else None
-                                    for c in [input_dim] + h + [None]])
+        self.layers = nn.ModuleList(nn.Linear(n, k) for n, k in zip([input_dim] + h, h + [output_dim]))
+        # self.layers = nn.ModuleList(nn.Conv1d(n, k, kernel_size=1) for n, k in zip([input_dim] + h, h + [output_dim]))
+        # self.norms = nn.ModuleList([nn.GroupNorm(c // 2, c) if c is not None else None
+        #                             for c in [input_dim] + h + [None]])
 
     def forward(self, x):
-        for i, (layer, norm) in enumerate(zip(self.layers, self.norms)):
-            x = F.relu(norm(layer(x))) if i < self.num_layers - 1 else layer(x)
+        for i, layer in enumerate(self.layers):
+            x = F.relu(layer(x)) if i < self.num_layers - 1 else layer(x)
         return x
 
 

4d3778b2c8624914a54b18d32a79606846148c85
damien911224 <damien911224@gmail.com>
2022-04-14 16:50:22 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -63,14 +63,14 @@ class RAFT(nn.Module):
                                                              dropout=0.1, activation="gelu",
                                                              n_levels=self.num_feature_levels * 2,
                                                              n_heads=8, n_points=4)
-                           for _ in range(1)))
+                           for _ in range(6)))
 
         self.keypoint_decoder = \
             nn.ModuleList((DeformableTransformerDecoderLayer(d_model=d_model, d_ffn=d_model * 4,
                                                              dropout=0.1, activation="gelu",
                                                              n_levels=self.num_feature_levels * 2,
                                                              n_heads=8, n_points=4, self_deformable=False)
-                           for _ in range(1)))
+                           for _ in range(6)))
 
         # self.keypoint_decoder = \
         #     nn.ModuleList((nn.TransformerDecoderLayer(d_model=d_model, dim_feedforward=d_model * 4,

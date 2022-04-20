f1823ee263414479f1b08f9df4b7f2442cb2df57
damien911224 <damien911224@gmail.com>
2022-04-20 17:15:20 +0900

    Modified

--- train.py
+++ train.py
@@ -77,7 +77,7 @@ def sequence_loss(flow_preds, flow_gt, valid, gamma=0.8, max_flow=MAX_FLOW):
         sparse_i_loss = (sparse_flow * scale - sparse_gt).abs()
         sparse_loss += i_weight * (sparse_valid[..., None] * sparse_i_loss).mean()
 
-    loss = flow_loss + sparse_loss * 0.2
+    loss = flow_loss + sparse_loss
 
     epe = torch.sum((flow_preds[0][-1] - flow_gt)**2, dim=1).sqrt()
     epe = epe.view(-1)[dense_valid.view(-1)]

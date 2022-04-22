276c4824c21b699fa776eef8e7a13b9f7044d570
damien911224 <damien911224@gmail.com>
2022-04-23 02:44:18 +0900

    Modified

--- train_standard.sh
+++ train_standard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p checkpoints
-python -u train.py --name chairs-base-dab-sparse --stage chairs --validation chairs --gpus 0, 1 --num_steps 1000000 --batch_size 16 --lr 0.0001 --image_size 352 480 --wdecay 0.0001
+python -u train.py --name chairs-base-dab-sparse --stage chairs --validation chairs --gpus 0 --num_steps 1000000 --batch_size 16 --lr 0.0001 --image_size 352 480 --wdecay 0.0001
 #python -u train.py --name raft-chairs --stage chairs --validation chairs --gpus 0 1 --num_steps 100000 --batch_size 2 --lr 0.0004 --image_size 184 248 --wdecay 0.0001
 #python -u train.py --name raft-things --stage things --validation sintel --restore_ckpt checkpoints/raft-chairs.pth --gpus 0 1 --num_steps 100000 --batch_size 6 --lr 0.000125 --image_size 400 720 --wdecay 0.0001
 #python -u train.py --name raft-things --stage things --validation sintel --gpus 0 1 --num_steps 100000 --batch_size 16 --lr 0.0002 --image_size 416 736 --wdecay 0.0001

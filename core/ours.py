8d0313576d5104cbc719d3943a9c575fcd18cd37
damien911224 <damien911224@gmail.com>
2022-04-22 20:30:25 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -472,9 +472,10 @@ class RAFT(nn.Module):
                     new_reference_points = (flow_embed + inverse_sigmoid(reference_points))
                     # key_flow = new_reference_points[..., :2].sigmoid().detach() - \
                     #            (new_reference_points[..., :2] + (new_reference_points[..., 2:])).sigmoid()
-                    key_flow = new_reference_points[..., :2].sigmoid().detach() - \
-                               (new_reference_points[..., :2].detach() + new_reference_points[..., 2:]).sigmoid()
-                    reference_points = new_reference_points.sigmoid().detach()
+                    key_flow = reference_points[..., :2].sigmoid().detach() - \
+                               (reference_points[..., :2].detach() + new_reference_points[..., 2:]).sigmoid()
+                    # reference_points = new_reference_points.sigmoid().detach()
+                    reference_points = new_reference_points.sigmoid()
 
                     # key_flow = inverse_sigmoid(reference_points.detach()) + flow_embed
                     # # key_flow = inverse_sigmoid(reference_points) + flow_embed

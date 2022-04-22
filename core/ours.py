4ead69ef5df3607938db8f164c206186ab511e29
damien911224 <damien911224@gmail.com>
2022-04-22 20:25:43 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -385,10 +385,10 @@ class RAFT(nn.Module):
             spatial_shapes = torch.as_tensor([feat.shape[2:] for feat in D1] * 2, dtype=torch.long, device=src.device)
             level_start_index = torch.cat((spatial_shapes.new_zeros((1,)), spatial_shapes.prod(1).cumsum(0)[:-1]))
 
-            # src_ref = self.get_reference_points(spatial_shapes, device=src.device)
-            # for i in range(len(self.encoder)):
-            #     src = self.encoder[i](src, src_pos, src_ref, spatial_shapes, level_start_index)
-            #
+            src_ref = self.get_reference_points(spatial_shapes, device=src.device)
+            for i in range(len(self.encoder)):
+                src = self.encoder[i](src, src_pos, src_ref, spatial_shapes, level_start_index)
+
             # dense_predictions = list()
             # for l_i in range(self.num_feature_levels):
             #     start_i = level_start_index[l_i]

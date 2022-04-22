d3494e45c2cc7f2b845ce883b66def2ee322fcd0
damien911224 <damien911224@gmail.com>
2022-04-22 16:20:52 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -59,7 +59,7 @@ class RAFT(nn.Module):
                 nn.GroupNorm(32, self.d_model)))
         self.input_proj = nn.ModuleList(input_proj_list)
 
-        self.encoder_iterations = 0
+        self.encoder_iterations = 6
         self.outer_iterations = 6
         self.inner_iterations = 1
         # self.num_keypoints = 10 ** 2
@@ -484,7 +484,7 @@ class RAFT(nn.Module):
                     scores = torch.max(context_flow, dim=1)[0]
                     # context_flow = torch.sigmoid(torch.bmm(U1, context.permute(0, 2, 1)))
                     # bs, HW, 2
-                    context_flow = torch.bmm(context_flow, key_flow)
+                    context_flow = torch.bmm(context_flow, key_flow.detach())
                     # bs, 2, H, W
                     context_flow = context_flow.permute(0, 2, 1).view(bs, 2, H, W)
 

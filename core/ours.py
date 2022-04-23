e3b17755e3c33055f06630deba185365501f5248
damien911224 <damien911224@gmail.com>
2022-04-24 00:27:14 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -62,7 +62,7 @@ class RAFT(nn.Module):
         self.input_proj = nn.ModuleList(input_proj_list)
 
         self.encoder_iterations = 1
-        self.outer_iterations = 3
+        self.outer_iterations = 6
         self.inner_iterations = 1
         # self.inner_iterations = self.num_feature_levels
         # self.num_keypoints = 10 ** 2
@@ -515,7 +515,7 @@ class RAFT(nn.Module):
                 scores = torch.max(context_flow, dim=1)[0].detach()
                 # context_flow = torch.sigmoid(torch.bmm(U1, context.permute(0, 2, 1)))
                 # bs, HW, 2
-                context_flow = torch.bmm(context_flow, key_flow.detach())
+                context_flow = torch.bmm(context_flow, key_flow)
                 # context_flow = torch.bmm(context_flow, key_flow.detach())
                 # bs, 2, H, W
                 context_flow = context_flow.permute(0, 2, 1).view(bs, 2, H, W)

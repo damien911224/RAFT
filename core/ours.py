af44fedff38a55eed841fefe9cbb0d19d50e2162
damien911224 <damien911224@gmail.com>
2022-04-24 22:43:22 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -405,26 +405,26 @@ class RAFT(nn.Module):
         for i in range(len(self.encoder)):
             src = self.encoder[i](src, src_pos, src_ref, spatial_shapes, level_start_index)
 
-        # dense_predictions = list()
-        # for l_i in range(self.num_feature_levels):
-        #     start_i = level_start_index[l_i]
-        #     this_H, this_W = spatial_shapes[l_i]
-        #     this_src_01 = src[:, start_i:start_i + this_H * this_W]
-        #     start_i = level_start_index[self.num_feature_levels + l_i]
-        #     this_H, this_W = spatial_shapes[self.num_feature_levels + l_i]
-        #     this_src_02 = src[:, start_i:start_i + this_H * this_W]
-        #     # bs, hw, hw
-        #     corr = F.softmax(torch.bmm(this_src_01, this_src_02.permute(0, 2, 1)), dim=-1)
-        #     # bs, hw, 2
-        #     this_coords = src_ref.squeeze(2)[:, start_i:start_i + this_H * this_W].repeat(bs, 1, 1)
-        #     dense_flow = this_coords - torch.bmm(corr, this_coords)
-        #     dense_flow = dense_flow.permute(0, 2, 1).view(bs, 2, this_H, this_W)
-        #     # flow_embed = self.flow_embed[l_i](this_src)[..., :2]
-        #     # dense_flow = flow_embed.tanh().permute(0, 2, 1).view(bs, 2, this_H, this_W)
-        #     dense_flow = dense_flow * \
-        #                  torch.as_tensor((I_W, I_H), dtype=torch.float32, device=src.device).view(1, 2, 1, 1)
-        #     dense_flow = F.interpolate(dense_flow, size=(I_H, I_W), mode="bilinear", align_corners=False)
-        #     dense_predictions.append(dense_flow)
+        dense_predictions = list()
+        for l_i in range(self.num_feature_levels):
+            start_i = level_start_index[l_i]
+            this_H, this_W = spatial_shapes[l_i]
+            this_src_01 = src[:, start_i:start_i + this_H * this_W]
+            start_i = level_start_index[self.num_feature_levels + l_i]
+            this_H, this_W = spatial_shapes[self.num_feature_levels + l_i]
+            this_src_02 = src[:, start_i:start_i + this_H * this_W]
+            # bs, hw, hw
+            corr = F.softmax(torch.bmm(this_src_01, this_src_02.permute(0, 2, 1)), dim=-1)
+            # bs, hw, 2
+            this_coords = src_ref.squeeze(2)[:, start_i:start_i + this_H * this_W].repeat(bs, 1, 1)
+            dense_flow = this_coords - torch.bmm(corr, this_coords)
+            dense_flow = dense_flow.permute(0, 2, 1).view(bs, 2, this_H, this_W)
+            # flow_embed = self.flow_embed[l_i](this_src)[..., :2]
+            # dense_flow = flow_embed.tanh().permute(0, 2, 1).view(bs, 2, this_H, this_W)
+            dense_flow = dense_flow * \
+                         torch.as_tensor((I_W, I_H), dtype=torch.float32, device=src.device).view(1, 2, 1, 1)
+            dense_flow = F.interpolate(dense_flow, size=(I_H, I_W), mode="bilinear", align_corners=False)
+            dense_predictions.append(dense_flow)
 
         dense_predictions = list()
 

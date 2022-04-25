526604d4c84cba83e96ecaf41c0e847302ca5867
damien911224 <damien911224@gmail.com>
2022-04-25 19:47:33 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -359,7 +359,7 @@ class RAFT(nn.Module):
             for i_i in range(self.inner_iterations):
                 if o_i >= 1:
                     N = self.num_keypoints * (2 ** (o_i - 1))
-                    reference_points.shape
+                    print(reference_points.shape)
                     reference_points = reference_points[:, :, 0].permute(0, 2, 1)
                     reference_points = reference_points.reshape(bs, 2, N, N)
                     reference_points = F.interpolate(reference_points, (N * 2, N * 2),

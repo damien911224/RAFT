84095b3872a24de41a8c19a6e5bb00214da82c86
damien911224 <damien911224@gmail.com>
2022-04-26 00:32:59 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -61,8 +61,8 @@ class RAFT(nn.Module):
         self.outer_iterations = 6
         self.inner_iterations = 1
         # self.inner_iterations = self.num_feature_levels
-        # self.num_keypoints = 100
-        self.num_keypoints = 25
+        self.num_keypoints = 100
+        # self.num_keypoints = 25
 
         self.encoder = \
             nn.ModuleList((DeformableTransformerEncoderLayer(d_model=self.d_model, d_ffn=self.d_model * 4,
@@ -388,31 +388,31 @@ class RAFT(nn.Module):
         root = round(math.sqrt(self.num_keypoints))
         base_reference_points = self.get_reference_points([(root, root), ], device=src.device).squeeze(2)
         base_reference_points = base_reference_points.repeat(bs, 1, 1)
-        reference_points = base_reference_points.detach().unsqueeze(2).repeat(1, 1, 2, 1)
+        reference_points = base_reference_points.detach().unsqueeze(2).repeat(1, 1, self.num_feature_levels * 2, 1)
         reference_flows = torch.zeros(dtype=torch.float32, size=(bs, self.num_keypoints, 2), device=src.device) + 0.5
         context_flow = torch.zeros(dtype=torch.float32, size=(bs, H * W, 2), device=src.device)
         for o_i in range(self.outer_iterations):
             for i_i in range(self.inner_iterations):
-                if o_i >= 1:
-                    step = 1
-                    N = round(math.sqrt(self.num_keypoints)) + ((o_i - 1) * step)
-                    reference_points = reference_points[:, :, 0].permute(0, 2, 1)
-                    reference_points = reference_points.reshape(bs, 2, N, N)
-                    reference_points = F.interpolate(reference_points, (N + step, N + step),
-                                                     mode="bilinear", align_corners=False)
-                    reference_points = reference_points.flatten(2).permute(0, 2, 1)
-                    reference_points = reference_points.unsqueeze(2).repeat(1, 1, self.num_feature_levels * 2, 1)
-
-                    query = query.permute(0, 2, 1)
-                    query = query.reshape(bs, self.d_model, N, N)
-                    query = F.interpolate(query, (N + step, N + step), mode="bilinear", align_corners=False)
-                    query = query.flatten(2).permute(0, 2, 1)
-
-                    reference_flows = reference_flows.permute(0, 2, 1)
-                    reference_flows = reference_flows.reshape(bs, 2, N, N)
-                    reference_flows = F.interpolate(reference_flows, (N + step, N + step),
-                                                    mode="bilinear", align_corners=False)
-                    reference_flows = reference_flows.flatten(2).permute(0, 2, 1)
+                # if o_i >= 1:
+                #     step = 1
+                #     N = round(math.sqrt(self.num_keypoints)) + ((o_i - 1) * step)
+                #     reference_points = reference_points[:, :, 0].permute(0, 2, 1)
+                #     reference_points = reference_points.reshape(bs, 2, N, N)
+                #     reference_points = F.interpolate(reference_points, (N + step, N + step),
+                #                                      mode="bilinear", align_corners=False)
+                #     reference_points = reference_points.flatten(2).permute(0, 2, 1)
+                #     reference_points = reference_points.unsqueeze(2).repeat(1, 1, self.num_feature_levels * 2, 1)
+                #
+                #     query = query.permute(0, 2, 1)
+                #     query = query.reshape(bs, self.d_model, N, N)
+                #     query = F.interpolate(query, (N + step, N + step), mode="bilinear", align_corners=False)
+                #     query = query.flatten(2).permute(0, 2, 1)
+                #
+                #     reference_flows = reference_flows.permute(0, 2, 1)
+                #     reference_flows = reference_flows.reshape(bs, 2, N, N)
+                #     reference_flows = F.interpolate(reference_flows, (N + step, N + step),
+                #                                     mode="bilinear", align_corners=False)
+                #     reference_flows = reference_flows.flatten(2).permute(0, 2, 1)
 
                 if self.use_dab:
                     raw_query_pos = torch.cat((reference_points[:, :, 0], reference_flows), dim=-1)

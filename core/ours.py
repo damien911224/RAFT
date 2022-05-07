15421a9db0a07742ff72c4595bbff127766304ea
damien911224 <damien911224@gmail.com>
2022-05-08 02:37:55 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -86,8 +86,8 @@ class RAFT(nn.Module):
             corr_proj_list.append(MLP(in_channels, self.d_model // 2, self.d_model // 2, 3))
         self.corr_proj = nn.ModuleList(corr_proj_list)
 
-        self.encoder_iterations = 3
-        self.outer_iterations = 6
+        self.encoder_iterations = 1
+        self.outer_iterations = 12
         self.inner_iterations = 1
         # self.inner_iterations = self.num_feature_levels
         self.num_keypoints = 256

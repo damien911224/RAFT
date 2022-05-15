f23953cff30717cd786c88bfdcd07c4d4c11c422
damien911224 <damien911224@gmail.com>
2022-05-15 19:26:00 +0900

    Modified

--- train_standard.sh
+++ train_standard.sh
@@ -4,7 +4,7 @@ mkdir -p checkpoints
 # 1: separate_none
 # 2: separate_query_motion_context
 # 3: separate_query
-python -u train.py --name chairs-base --stage chairs --validation chairs --gpus 0 --num_steps 1000000 --batch_size 16 --lr 0.0002 --image_size 352 480 --wdecay 0.0001
+python -u train.py --name chairs-base --stage chairs --validation chairs --gpus 0 1 --num_steps 1000000 --batch_size 16 --lr 0.0002 --image_size 352 480 --wdecay 0.0001
 #python -u train.py --name raft-things --stage things --validation sintel --restore_ckpt checkpoints/raft-chairs.pth --gpus 0 1 --num_steps 100000 --batch_size 6 --lr 0.000125 --image_size 400 720 --wdecay 0.0001
 #python -u train.py --name sintel-base --stage sintel --validation sintel --gpus 0 --num_steps 1000000 --batch_size 16 --lr 0.0001 --image_size 352 768 --wdecay 0.00001 --gamma=0.85
 #python -u train.py --name raft-sintel --stage sintel --validation sintel --restore_ckpt checkpoints/raft-things.pth --gpus 0 1 --num_steps 100000 --batch_size 6 --lr 0.000125 --image_size 368 768 --wdecay 0.00001 --gamma=0.85

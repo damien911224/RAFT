6c40c8015e51a3665fa924cd1c41029654da72b6
damien911224 <damien911224@gmail.com>
2022-05-18 23:35:40 +0900

    Modified

--- train_standard.sh
+++ train_standard.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p checkpoints
 # 0: base_lateral_decoder
-# 1: base_accumulate_masks
-# 2: base_src
+# 1: base_inner
+# 2: base_fixed
 # 3: base
 python -u train.py --name chairs-base --stage chairs --validation chairs --gpus 0 --num_steps 1000000 --batch_size 16 --lr 0.0002 --image_size 352 480 --wdecay 0.0001
 #python -u train.py --name raft-things --stage things --validation sintel --restore_ckpt checkpoints/raft-chairs.pth --gpus 0 1 --num_steps 100000 --batch_size 6 --lr 0.000125 --image_size 400 720 --wdecay 0.0001

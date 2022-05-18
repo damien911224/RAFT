4d5a755b4254116cb4a23f1df632cb69563129ec
damien911224 <damien911224@gmail.com>
2022-05-18 23:31:26 +0900

    Modified

--- core/ours.py
+++ core/ours.py
@@ -295,7 +295,7 @@ class RAFT(nn.Module):
         nn.init.normal_(self.img_pos_embed.weight)
         nn.init.normal_(self.row_pos_embed.weight)
         nn.init.normal_(self.col_pos_embed.weight)
-        nn.init.normal_(self.iter_pos_embed.weight)
+        # nn.init.normal_(self.iter_pos_embed.weight)
 
     def _get_clones(self, module, N):
         return nn.ModuleList([copy.deepcopy(module) for i in range(N)])

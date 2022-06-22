84a498c16e45728eb23d52cec0297bfc6c8ec843
damien911224 <damien911224@gmail.com>
2022-06-22 14:36:50 -0500

    Modified

--- train_standard.sh
+++ train_standard.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p checkpoints
 # 0: base_things
-# 1: base_S4_inner_U2
+# 1: base_chairs
 # 2: base_things
 # 3: base_S4_inner
 #python -u train.py --name chairs-base --stage chairs --validation chairs --gpus 0 --num_steps 1000000 --batch_size 10 --lr 0.0002 --image_size 352 480 --wdecay 0.0001
